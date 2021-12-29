1a780c9c859d82125ba81a8d3d32a0caf5a6f879
FinnRG <finngaertner@protonmail.com>
2021-12-29 22:13:49 +0000

    feat(Frontend): Implement basic upload functions

--- frontend/src/components/upload/FileUpload.js
+++ frontend/src/components/upload/FileUpload.js
@@ -1,28 +1,11 @@
-import axios from 'axios';
-import { useState } from 'react';
 import { Form } from 'react-bulma-components';
 const { InputFile } = Form;
 
-const FileUpload = () => {
+const FileUpload = (props) => {
 
-    let [file, setFile] = useState(null);
-
-    const upload = (file) => {
-        axios.post('http://localhost:8000/upload/' + file.name, file, {
-            headers: {
-                'Content-Type': file.type,
-                'Authorization': 'Accept',
-                'Access-Control-Allow-Origin': true,
-            }
-        })
-    }
-
-    return <main>
-        <InputFile value={file} onChange={(e) => {
-            setFile(e.target.files);
-            upload(e.target.files.item(0));
-        }} />
-    </main>
+    return <InputFile value={props.value} onChange={(e) => {
+        props.setValue(e.target.files);
+    }} />
 }
 
 export default FileUpload;
--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -2,14 +2,35 @@ import { faAlignLeft, faHeading } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import { Form } from 'react-bulma-components';
 import FormInputField from '../components/shared/FormInputField';
+import FormSubmitButton from '../components/shared/FormSubmitButton';
 import FileUpload from '../components/upload/FileUpload';
+import { client } from '../App';
 
 const { Field } = Form;
 
 const Upload = () => {
 
+    const [file, setFile] = useState(null);
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    const handleSubmit = (e) => {
+
+        client({
+            method: 'post',
+            url: 'http://localhost:8000/upload/' + title,
+            data: file[0],
+            params: {
+                description,
+            },
+            withCredentials: true,
+            headers: { 'Content-Type': file.type }
+        })
+            .then((resp) => setSuccess(true))
+            .catch((err) => console.log(err))
+        e.preventDefault();
+    }
 
     return <form>
         <Field>
@@ -17,8 +38,9 @@ const Upload = () => {
             <FormInputField label={"Description"} value={description} setValue={setDescription} icon={faAlignLeft} />
         </Field>
         <Field className="mb-1">
-            <FileUpload />
+            <FileUpload value={file} setValue={setFile} />
         </Field>
+        <FormSubmitButton submit={handleSubmit} setters={[setFile, setTitle, setDescription]} />
     </form>
 }
 
