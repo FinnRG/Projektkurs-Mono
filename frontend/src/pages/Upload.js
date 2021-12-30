6928dbbafb5bd21ed29c02523e15c3ebf86ca273
FinnRG <finngaertner@protonmail.com>
2021-12-30 18:06:35 +0000

    feat(Frontend): IMplement a basic progress bar

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -1,6 +1,6 @@
 import { faAlignLeft, faHeading } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
-import { Form } from 'react-bulma-components';
+import { Form, Progress } from 'react-bulma-components';
 import FormInputField from '../components/shared/FormInputField';
 import FormSubmitButton from '../components/shared/FormSubmitButton';
 import FileUpload from '../components/upload/FileUpload';
@@ -14,6 +14,8 @@ const Upload = () => {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [success, setSuccess] = useState(false);
+    const [progress, setProgress] = useState(null);
+
 
     const handleSubmit = (e) => {
 
@@ -24,6 +26,13 @@ const Upload = () => {
             },
             headers: {
                 'Content-Type': file.type,
+            },
+            // https://stackoverflow.com/questions/44936028/progress-bar-with-axios
+            onUploadProgress: (progressEvent) => {
+                const totalLength = progressEvent.lengthComputable ? progressEvent.total : progressEvent.target.getResponseHeader('content-length') || progressEvent.target.getResponseHeader('x-decompressed-content-length');
+                if (totalLength !== null) {
+                    setProgress(Math.round((progressEvent.loaded * 100) / totalLength));
+                }
             }
         })
             .then((_) => setSuccess(true))
@@ -40,6 +49,9 @@ const Upload = () => {
             <FileUpload setValue={setFile} />
         </Field>
         <FormSubmitButton submit={handleSubmit} setters={[setFile, setTitle, setDescription]} />
+        {progress && (
+            <Progress max={100} value={progress} />
+        )}
     </form>
 }
 
