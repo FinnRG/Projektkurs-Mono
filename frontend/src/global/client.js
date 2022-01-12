301c43f35b000ca716f9fe3260003508db76b1f7
FinnRG <finngaertner@protonmail.com>
2022-01-12 22:33:20 +0100

    fix(gitpod): Various smaller gitpod fixes

--- frontend/src/global/client.js
+++ frontend/src/global/client.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 import { wrapper } from 'axios-cookiejar-support';
 import { CookieJar } from 'tough-cookie';
 
+const process = require('process');
+
 const jar = new CookieJar();
-const client = wrapper(axios.create({ jar, withCredentials: true }));
+const client = wrapper(axios.create({
+    jar,
+    withCredentials: true,
+    baseURL: process.env.GITPOD_WORKSPACE_URL || 'http://localhost:8000'
+}));
 export default client;
