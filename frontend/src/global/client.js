e06e18c73a7678eb31fe8bd0eb763bfc345be4e7
Pyther <finngaertner@protonmail.com>
2022-01-12 22:39:36 +0000

    fix(gitpod): Use gitpod env

--- frontend/src/global/client.js
+++ frontend/src/global/client.js
@@ -2,12 +2,16 @@ import axios from 'axios';
 import { wrapper } from 'axios-cookiejar-support';
 import { CookieJar } from 'tough-cookie';
 
-const process = require('process');
-
 const jar = new CookieJar();
+
+let pod_url = process.env.REACT_APP_WORKSPACE_URL;
+if (pod_url) {
+    pod_url = [pod_url.slice(0, 8), '8000-', pod_url.slice(8)].join('');
+}
+
 const client = wrapper(axios.create({
     jar,
     withCredentials: true,
-    baseURL: process.env.GITPOD_WORKSPACE_URL || 'http://localhost:8000'
+    baseURL: pod_url || 'http://localhost:8000'
 }));
 export default client;
