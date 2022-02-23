c89cdebb0f032196b77c0b7619c45a19453fecd8
FinnRG <finngaertner@protonmail.com>
2022-02-23 17:59:39 +0100

    feat: Show description in VideoList

--- frontend/src/components/list/VideoLink.js
+++ frontend/src/components/list/VideoLink.js
@@ -1,19 +1,27 @@
-import { Card } from 'react-bulma-components';
+import { Card, Columns } from 'react-bulma-components';
 import { Link } from 'react-router-dom';
 import Trash from '../shared/Trash';
-
-const { Content } = Card;
 
 const VideoLink = (props) => {
   return (
     <>
       <Card>
-        <Content>
-          {props.tagView && <Trash onClick={() => props.trash()} />}
-          <Link className='ml-4' to={`/player/${props.id}`}>
-            {props.title}{' '}
-          </Link>
-        </Content>
+        <Card.Content>
+          <Columns>
+            {props.tagView && (
+              <Columns.Column className='is-narrow'>
+                <Trash onClick={() => props.trash()} />
+              </Columns.Column>
+            )}
+            <Columns.Column>
+              <Link className='is-size-5' to={`/player/${props.id}`}>
+                {props.title}{' '}
+              </Link>
+              <br />
+              {props.description}
+            </Columns.Column>
+          </Columns>
+        </Card.Content>
       </Card>
     </>
   );
