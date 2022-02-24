3632cf81311b76eb14aa070200f190ee375bfdf0
FinnRG <finngaertner@protonmail.com>
2022-02-24 12:28:13 +0100

    feat(frontend): Install react-toastify

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -1,6 +1,9 @@
 import { Box } from 'react-bulma-components';
 import { Outlet } from 'react-router-dom';
 import Header from './components/shared/Header';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.min.css';
 
 export const Layout = (props) => {
   return (
@@ -8,6 +11,7 @@ export const Layout = (props) => {
       <Box>
         <Header />
         <Outlet />
+        <ToastContainer />
       </Box>
     </div>
   );
