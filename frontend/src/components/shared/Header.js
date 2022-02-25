f44a7f7346162b9206bd23c0bfd1a98e50ac742f
FinnRG <finngaertner@protonmail.com>
2022-02-25 11:41:28 +0000

    fix(frontend): Transform burger to cross if active

--- frontend/src/components/shared/Header.js
+++ frontend/src/components/shared/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
     <>
       <Navbar mb={showMenu ? 4 : 0} transparent={true}>
         <Navbar.Brand>
-          <Navbar.Burger onClick={() => setShowMenu(!showMenu)} />
+          <Navbar.Burger className={showMenu ? 'is-active' : ''} onClick={() => setShowMenu(!showMenu)} />
         </Navbar.Brand>
 
         <Navbar.Menu className={showMenu ? 'is-active' : ''}>
