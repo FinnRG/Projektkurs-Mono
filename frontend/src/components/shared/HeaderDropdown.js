058911dcf0299d67f2c3f9eacbabd6ced101af61
FinnRG <finngaertner@protonmail.com>
2022-02-25 14:51:57 +0000

    fix(frontend): Close dropdown if click elsewhere

--- frontend/src/components/shared/HeaderDropdown.js
+++ frontend/src/components/shared/HeaderDropdown.js
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar } from "react-bulma-components";
 import { useLocation } from "react-router";
+import useOnClickOutside from 'use-onclickoutside';
 
 const HeaderDropdown = (props) => {
 
     const [active, setActive] = useState(false);
+    const ref = React.useRef(null);
+    useOnClickOutside(ref, () => setActive(false));
 
     const location = useLocation();
 
@@ -12,7 +15,7 @@ const HeaderDropdown = (props) => {
         setActive(false);
     }, [location]);
 
-    return <div className="navbar-item p-0 m-0" tabIndex={0} onBlur={() => setActive(false)}>
+    return <div ref={ref} className="navbar-item p-0 m-0">
         <Navbar.Item className={active ? 'is-active' : ''}>
             <Navbar.Link onClick={() => setActive(!active)}>
                 {props.text}
