e316b3a9a1119f5deb34912e0969007585226bd4
Finn R. Gärtner <65015656+FinnRG@users.noreply.github.com>
2022-03-15 19:39:00 +0100

    fix: Use nginx (#34)
    * fix(CI): Use react production build
    
    * fix(CI): Reduce redis replicaCount
    
    * fix(CI): Use proper kube dns urls
    
    * fix(CI): Use nginx

--- frontend/src/components/player/StaticPlayer.js
+++ frontend/src/components/player/StaticPlayer.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactHlsPlayer from 'react-hls-player';
-import { pod_url } from '../../global/client';
 
 const StaticPlayer = (props) => {
   return (
     <ReactHlsPlayer
-      src={pod_url + '/get/' + props.video_id}
+      src={'/api/get/' + props.video_id}
       autoPlay={false}
       controls={true}
       className='mr-6'
