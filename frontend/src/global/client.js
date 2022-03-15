7bf66ebd6ded3ffed32923dc08e69303de465470
Finn R. Gärtner <65015656+FinnRG@users.noreply.github.com>
2022-03-15 13:02:37 +0100

    feat: Use Okteto-Cloud (#31)
    * feat(okteto): Set up okteto pipeline
    
    * fix: Specify storage claim size
    
    * fix: Update storage claim sizes
    
    * feat(CI): Dockerize frontend
    
    * fix(CI): Remove gitpod support
    
    * fix(CI): Remove devcontainer support
    
    * fix(CI): Properly implement okteto
    
    * fix(CI): Properly connect frontend with backend
    
    * fix(CI): Fix typo in image name
    
    * fix(CI): Use a deploymeent instead of a pod
    
    * feat(CI): Add okteto preview deployment as action

--- frontend/src/global/client.js
+++ frontend/src/global/client.js
@@ -4,7 +4,7 @@ import { CookieJar } from 'tough-cookie';
 
 const jar = new CookieJar();
 
-let pod_url = process.env.REACT_APP_WORKSPACE_URL;
+let pod_url = process.env.API_URl;
 if (pod_url) {
   pod_url = [pod_url.slice(0, 8), '8000-', pod_url.slice(8)].join('');
 } else {
