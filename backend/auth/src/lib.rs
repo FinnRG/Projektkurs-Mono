51bce289bd523013c00016ec8aebfd39f2b4a804
Finn R. Gärtner <finngaertner@protonmail.com>
2022-03-17 19:35:38 +0100

    feat: First version of auth service

---
+++ backend/auth/src/lib.rs
@@ -0,0 +1,15 @@
+pub mod rpc {
+    tonic::include_proto!("auth");
+}
+
+pub mod db {
+    use msostream::{establish_connection, user};
+
+    pub fn create_user(name: &str, email: &str, password: &str) ->String {
+        let id = uuid::Uuid::new_v4().to_string();
+        user::create_user(&establish_connection(), &id, name, email, password);
+        id
+    }
+}
+
+pub fn test() {}
---
+++ backend/auth/src/service.rs
@@ -0,0 +1,108 @@
+use std::{env, str::Bytes};
+
+use auth_lib::db::create_user;
+use auth_lib::rpc::{
+    auth_server::{Auth, AuthServer},
+    login_response::ResponseType,
+    EncodedJwt, LoginRequest, LoginResponse, RegisterRequest,
+};
+use chrono::{Duration, Local};
+use jsonwebtoken::{encode, Algorithm, EncodingKey, Header};
+use lazy_static::lazy_static;
+use log::{error, trace};
+use msostream::establish_connection;
+use msostream::user::check_password;
+use serde::{Deserialize, Serialize};
+use tonic::{transport::Server, Request, Response, Status};
+
+lazy_static! {
+    static ref JWT_SECRET: String = env::var("JWT_SECRET").unwrap();
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub: String,
+    exp: i64,
+}
+
+fn get_exp() -> i64 {
+    (Local::now() + Duration::days(1)).timestamp()
+}
+
+impl Claims {
+    fn new(id: String) -> Self {
+        Claims {
+            sub: id,
+            exp: get_exp(),
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct Authenticator {}
+
+#[tonic::async_trait]
+impl Auth for Authenticator {
+    async fn register(
+        &self,
+        request: Request<RegisterRequest>,
+    ) -> Result<Response<EncodedJwt>, Status> {
+        let user = request.into_inner();
+        let id = create_user(&user.name, &user.email, &user.password);
+        let claims = Claims::new(id);
+
+        let jwt = match encode(
+            &Header::default(),
+            &claims,
+            &EncodingKey::from_secret(JWT_SECRET.as_bytes()),
+        ) {
+            Ok(c) => c,
+            Err(err) => panic!(),
+        };
+
+        Ok(Response::new(EncodedJwt { jwt }))
+    }
+
+    async fn login(
+        &self,
+        request: Request<LoginRequest>,
+    ) -> Result<Response<LoginResponse>, Status> {
+        let cred = request.into_inner();
+        match check_password(&establish_connection(), &cred.email, &cred.password) {
+            Some(id) => {
+                let claims = Claims::new(id);
+
+                let jwt = match encode(
+                    &Header::default(),
+                    &claims,
+                    &EncodingKey::from_secret(JWT_SECRET.as_bytes()),
+                ) {
+                    Ok(c) => c,
+                    Err(err) => panic!(),
+                };
+
+                Ok(Response::new(LoginResponse {
+                    jwt,
+                    result: ResponseType::Success as i32,
+                }))
+            }
+            _ => Err(Status::aborted("T")),
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    pretty_env_logger::env_logger::init();
+    msostream::run_db_migrations();
+
+    let addr = "0.0.0.0:50051".parse()?;
+    let authenticator = Authenticator::default();
+
+    Server::builder()
+        .add_service(AuthServer::new(authenticator))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
