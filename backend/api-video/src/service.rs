5644ba5f888b8bb5b575c671628711b8353ff3ba
FinnRG <finngaertner@protonmail.com>
2022-04-18 17:37:22 +0200

    feat: Implement GetVideo

--- backend/api-video/src/service.rs
+++ backend/api-video/src/service.rs
@@ -5,21 +5,25 @@ use rdkafka::{
     consumer::{CommitMode, Consumer, DefaultConsumerContext, StreamConsumer},
     ClientConfig, Message,
 };
-
-include!("../gen/mod.rs");
-
-use tonic::{transport::Server, Request, Response, Status};
+use redis::Commands;
+use std::env;
+use tonic::{codegen::http::request, transport::Server, Request, Response, Status};
 use videos::v1::video_service_server::{VideoService, VideoServiceServer};
+use videos::v1::Video;
 use videos::v1::{
     CreateVideoRequest, CreateVideoResponse, GetVideoRequest, GetVideoResponse, UpdateVideoRequest,
     UpdateVideoResponse,
 };
-
+#[macro_use]
+extern crate rs_auth;
 extern crate r2d2;
 #[macro_use]
 extern crate lazy_static;
 
+include!("../gen/mod.rs");
+
 lazy_static! {
+    static ref JWTSECRET: String = env::var("JWTSECRET").unwrap();
     static ref POOL: r2d2::Pool<redis::Client> = {
         let client = redis::Client::open("redis://127.0.0.1/").unwrap();
         r2d2::Pool::<redis::Client>::builder()
@@ -60,6 +64,16 @@ lazy_static! {
 //         }
 //     }
 
+fn get_conn() -> Result<r2d2::PooledConnection<redis::Client>, Status> {
+    match POOL.get() {
+        Ok(conn) => Ok(conn),
+        Err(e) => {
+            log::error!("{:?}", e);
+            Err(Status::internal("Failed to connect to Redis"))
+        }
+    }
+}
+
 #[derive(Debug, Default)]
 pub struct Videos {}
 
@@ -69,7 +83,23 @@ impl VideoService for Videos {
         &self,
         request: Request<GetVideoRequest>,
     ) -> Result<Response<GetVideoResponse>, Status> {
-        Err(Status::aborted("T"))
+        let id = request.into_inner().id;
+
+        let mut conn = get_conn()?;
+
+        return match conn.get::<_, Option<String>>(&id) {
+            Ok(Some(video)) => {
+                let parsed: Video = serde_json::from_str(video.as_ref()).unwrap();
+                Ok(Response::new(GetVideoResponse {
+                    video: Some(parsed),
+                }))
+            }
+            Ok(None) => Err(Status::not_found(format!(
+                "Couldn't find video with id: {}",
+                id
+            ))),
+            Err(_) => Err(Status::internal("Internal Redis error")),
+        };
     }
 
     async fn update_video(
@@ -83,6 +113,13 @@ impl VideoService for Videos {
         &self,
         request: Request<CreateVideoRequest>,
     ) -> Result<Response<CreateVideoResponse>, Status> {
+        request.metadata().get("auth").unwrap().as_ref();
+        if !rs_auth::is_logged_in!(request) {
+            return Err(Status::unauthenticated("User is not logged in"));
+        }
+
+        let mut conn = get_conn()?;
+
         Err(Status::aborted("T"))
     }
 }
