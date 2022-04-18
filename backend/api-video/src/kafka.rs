678758639a9fcbedf5e507d69c8e9e73296074df
FinnRG <finngaertner@protonmail.com>
2022-04-18 19:03:40 +0200

    feat(backend): Emit VideoCreated event

---
+++ backend/api-video/src/kafka.rs
@@ -0,0 +1,56 @@
+use std::time::Duration;
+
+use rdkafka::{
+    message::OwnedHeaders,
+    producer::{future_producer::OwnedDeliveryResult, FutureProducer, FutureRecord},
+    ClientConfig,
+};
+
+// let context = DefaultConsumerContext::default();
+
+// let consumer: StreamConsumer = ClientConfig::new()
+//     .set("group.id", "video_api")
+//     .set("bootstrap.servers", "localhost:9092")
+//     .set("enable.partition.eof", "false")
+//     .set("session.timeout.ms", "6000")
+//     .set("enable.auto.commit", "true")
+//     .set_log_level(RDKafkaLogLevel::Debug)
+//     .create_with_context(context)
+//     .expect("Consumer creation failed");
+
+// consumer.subscribe(&vec!["upload"])
+//     .expect("Can't subscribe to upload");
+
+//     loop {
+//         match consumer.recv().await {
+//             Err(e) => warn!("Kafka error: {:?}", e),
+//             Ok(m) => {
+//                 let payload = match m.payload_view::<str>() {
+//                     None => "",
+//                     Some(Ok(s)) => s,
+//                     Some(Err(e)) => { warn!("Error while deserializing payload: {:?}", e); "" },
+//                 };
+//                 info!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
+//                   m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp());
+//                 consumer.commit_message(&m, CommitMode::Async).unwrap();
+//             }
+//         }
+//     }
+
+pub async fn emit_video_created(video: &str, id: &str) -> OwnedDeliveryResult {
+    let producer: &FutureProducer = &ClientConfig::new()
+        .set("bootstrap.server", "http://kafka-headless.default.svc.cluster.local:9092")
+        .set("message.timeout.ms", "5000")
+        .create()
+        .expect("Producer creation error");
+
+    producer
+        .send(
+            FutureRecord::to("videos")
+                .payload(video)
+                .key(id)
+                .headers(OwnedHeaders::new().add("type", "VideoCreated")),
+            Duration::from_secs(0),
+        )
+        .await
+}
--- backend/api-video/src/service.rs
+++ backend/api-video/src/service.rs
@@ -1,25 +1,19 @@
-use log::{info, warn};
-use r2d2::Pool;
-use rdkafka::{
-    config::RDKafkaLogLevel,
-    consumer::{CommitMode, Consumer, DefaultConsumerContext, StreamConsumer},
-    ClientConfig, Message,
-};
+use log::error;
 use redis::Commands;
 use std::env;
-use tonic::{codegen::http::request, transport::Server, Request, Response, Status};
+use tonic::{transport::Server, Request, Response, Status};
 use videos::v1::video_service_server::{VideoService, VideoServiceServer};
 use videos::v1::Video;
 use videos::v1::{
     CreateVideoRequest, CreateVideoResponse, GetVideoRequest, GetVideoResponse, UpdateVideoRequest,
     UpdateVideoResponse, Visibility,
 };
-extern crate r2d2;
-extern crate rs_auth;
 #[macro_use]
 extern crate lazy_static;
 
 include!("../gen/mod.rs");
+
+mod kafka;
 
 lazy_static! {
     static ref JWTSECRET: String = env::var("JWTSECRET").unwrap();
@@ -32,42 +26,11 @@ lazy_static! {
     };
 }
 
-// let context = DefaultConsumerContext::default();
-
-// let consumer: StreamConsumer = ClientConfig::new()
-//     .set("group.id", "video_api")
-//     .set("bootstrap.servers", "localhost:9092")
-//     .set("enable.partition.eof", "false")
-//     .set("session.timeout.ms", "6000")
-//     .set("enable.auto.commit", "true")
-//     .set_log_level(RDKafkaLogLevel::Debug)
-//     .create_with_context(context)
-//     .expect("Consumer creation failed");
-
-// consumer.subscribe(&vec!["upload"])
-//     .expect("Can't subscribe to upload");
-
-//     loop {
-//         match consumer.recv().await {
-//             Err(e) => warn!("Kafka error: {:?}", e),
-//             Ok(m) => {
-//                 let payload = match m.payload_view::<str>() {
-//                     None => "",
-//                     Some(Ok(s)) => s,
-//                     Some(Err(e)) => { warn!("Error while deserializing payload: {:?}", e); "" },
-//                 };
-//                 info!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?}",
-//                   m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp());
-//                 consumer.commit_message(&m, CommitMode::Async).unwrap();
-//             }
-//         }
-//     }
-
 fn get_conn() -> Result<r2d2::PooledConnection<redis::Client>, Status> {
     match POOL.get() {
         Ok(conn) => Ok(conn),
         Err(e) => {
-            log::error!("{:?}", e);
+            error!("{:?}", e);
             Err(Status::internal("Failed to connect to Redis"))
         }
     }
@@ -103,7 +66,7 @@ impl VideoService for Videos {
 
     async fn update_video(
         &self,
-        request: Request<UpdateVideoRequest>,
+        _request: Request<UpdateVideoRequest>,
     ) -> Result<Response<UpdateVideoResponse>, Status> {
         Err(Status::aborted("T"))
     }
@@ -113,7 +76,7 @@ impl VideoService for Videos {
         request: Request<CreateVideoRequest>,
     ) -> Result<Response<CreateVideoResponse>, Status> {
         let author = rs_auth::user_id!(request);
-        if !author.is_some() {
+        if author.is_none() {
             return Err(Status::unauthenticated("User is not logged in"));
         }
 
@@ -133,12 +96,20 @@ impl VideoService for Videos {
             date: chrono::offset::Local::now().to_string(),
             visibility: Visibility::Draft as i32,
         })
-        .unwrap();
+        .expect("Unable to stringify Video object");
 
-        match conn.set::<_, _, ()>(id.to_string(), video) {
-            Ok(_) => Ok(Response::new(CreateVideoResponse { id: id.to_string() })),
-            Err(_) => Err(Status::internal("Internal Redis error")),
+        if conn.set::<_, _, ()>(id.to_string(), &video).is_err() {
+            return Err(Status::internal("Internal Redis error"));
         }
+
+        if kafka::emit_video_created(&video, &id.to_string())
+            .await
+            .is_err()
+        {
+            return Err(Status::internal("Internal kafka error"));
+        }
+
+        Ok(Response::new(CreateVideoResponse { id: id.to_string() }))
     }
 }
 
