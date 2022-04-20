5413f3834274db57f4868c71244d8925cdfbe1ed
FinnRG <finngaertner@protonmail.com>
2022-04-20 17:13:14 +0200

    fix: Use JWTSECRET globally

--- backend/auth/src/lib.rs
+++ backend/auth/src/lib.rs
@@ -5,7 +5,7 @@ use rs_auth::Claims;
 use std::env;
 
 lazy_static! {
-    static ref JWT_SECRET: String = env::var("JWT_SECRET").unwrap();
+    static ref JWT_SECRET: String = env::var("JWTSECRET").unwrap();
 }
 
 pub fn create_jwt(id: String) -> String {
